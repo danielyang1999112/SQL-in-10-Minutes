872c560f65793b0016b395d7e0a90890b8fe35a0
Daniel Yang <59007175+danielyang1999112@users.noreply.github.com>
2022-01-01 13:53:17 +0800

    Add files via upload

---
+++ SQL in 10 Minutes/4 Filtering Data.sql
@@ -0,0 +1,55 @@
+-- SELECT prod_name, prod_price
+-- FROM products
+-- WHERE prod_price = 3.49;
+
+/*
+= <> !=
+< <= !<
+> >= !>
+BETWEEN
+IS NULL
+*/
+
+-- SELECT prod_name, prod_price
+-- FROM products
+-- WHERE prod_price < 10;
+-- WHERE prod_price <= 10;
+
+-- SELECT vend_id, prod_name
+-- FROM products
+-- WHERE vend_id <> 'DLL01';
+-- WHERE vend_id != 'DLL01';
+
+-- SELECT prod_name, prod_price
+-- FROM products
+-- WHERE prod_price BETWEEN 5 AND 10;
+
+-- SELECT prod_name
+-- FROM products
+-- WHERE prod_price IS NULL;
+
+-- SELECT cust_name
+-- FROM customers
+-- WHERE cust_email IS NULL;
+
+/* // Challenge // */
+-- 1 v
+SELECT prod_id, prod_name
+FROM products
+WHERE prod_price = 9.49;
+
+-- 2 v
+SELECT prod_id, prod_name
+FROM products
+WHERE prod_price >= 9;
+
+-- 3 v
+SELECT DISTINCT order_num
+FROM orderitems
+WHERE quantity >= 100;
+
+-- 4 v
+SELECT prod_name, prod_price
+FROM products
+WHERE prod_price BETWEEN 3 AND 6
+ORDER BY prod_price;
